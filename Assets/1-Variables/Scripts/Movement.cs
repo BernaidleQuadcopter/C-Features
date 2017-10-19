6e969882b06463a1e9b49441c937f72709325bfe
BernaidleQuadcopter <woody.willman2@gmial.com>
2017-10-20 09:24:17 +1100

    Uploading_C#Features

---
+++ Assets/1-Variables/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean up code: CTRL+K+D
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 20f;
+    public float rotationSpeed = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check IF user presses W
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Move Up
+            // 20 * 0.033 = 0.6
+            transform.Translate(new Vector3(0, movementSpeed, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Move Up
+            // 20 * 0.033 = 0.6
+            transform.Translate(new Vector3(0, -movementSpeed, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Move Up
+            // 20 * 0.033 = 0.6
+            transform.Translate(new Vector3(movementSpeed, 0 , 0) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Move Up
+            // 20 * 0.033 = 0.6
+            transform.Translate(new Vector3(-movementSpeed, 0, 0) * Time.deltaTime);
+        }
+        {
+
+        }
+    }
+}
---
+++ Assets/4-Arrays/Scripts/Powerups/Powerup_Speed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Arrays
+{
+    public class Powerup_Speed : MonoBehaviour
+    {
+        public float modifier = 2f;
+
+        // Collision Detection
+        void OnTriggerEnter(Collider col)
+        {
+            // Detect collision with player
+            Player p = col.GetComponent<Player>();
+            if(p != null)
+            {
+                // Add Modifier to force
+                p.force *= modifier;
+                // Destroyer powerup 
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
